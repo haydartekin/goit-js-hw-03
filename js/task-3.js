dbd2de40ec907b4a67dd66b3ad9e36063c81754f
Haydar Tekin <haydartekin1992@gmail.com>
2024-11-24 13:42:50 +0300

    JS Homework 4

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,27 @@
-console.log("- TASK-3 -")
+console.log("- TASK-3 -");
 
-function filterArray(numbers, value) {
-  const newNumbers = [];
-      for (let i = 0; i < numbers.length; i++) {
-          if (numbers[i] > value) {
-          newNumbers.push(numbers[i]);
-          }
-}
-  return newNumbers;
-}
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    const updatedTime = hours + this.playTime;
+    this.playTime = updatedTime;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
